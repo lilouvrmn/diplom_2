f8b8dbc210141df15bebe14960570266c33d9ee7
Julia Verman <lilou@vrmn.ru>
2024-06-12 15:38:22 +0300

    Добавила пом и код

---
+++ src/main/java/Order/Ingredients.java
@@ -0,0 +1,13 @@
+package Order;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class Ingredients {
+
+    private String[] ingredients;
+}
