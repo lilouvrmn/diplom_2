50b4e01007c362a87bf7cf935134cbb20cec27f8
Julia Verman <lilou@vrmn.ru>
2024-06-17 16:34:37 +0300

    Исправила код в IngredientsGenerator

--- src/main/java/order/IngredientsGenerator.java
+++ src/main/java/order/IngredientsGenerator.java
@@ -1,9 +1,15 @@
 package order;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IngredientsGenerator {
 
     public static Ingredients getIngredients() {
-        return new Ingredients(new String[]{"61c0c5a71d1f82001bdaaa6c", "61c0c5a71d1f82001bdaaa70"});
+        List<String> ingredientIds = new ArrayList<>();
+        ingredientIds.add("61c0c5a71d1f82001bdaaa6c");
+        ingredientIds.add("61c0c5a71d1f82001bdaaa70");
+        return new Ingredients(ingredientIds.toArray(new String[0]));
     }
 
     public static Ingredients getIngredientsEmpty() {
